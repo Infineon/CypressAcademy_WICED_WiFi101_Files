7a59e4be55bc6ed19b87faffd29850da5c8235f4
Greg Landry <gjl@cypress.com>
2017-10-30 15:57:08 -0400

    Update platform files for wiced 6.0

--- Platforms/WW101_2_CYW943907AEVAL1F/WW101_2_CYW943907AEVAL1F.mk
+++ Platforms/WW101_2_CYW943907AEVAL1F/WW101_2_CYW943907AEVAL1F.mk
@@ -1,7 +1,36 @@
 #
-# $ Copyright Broadcom Corporation $
+# Copyright 2017, Cypress Semiconductor Corporation or a subsidiary of 
+ # Cypress Semiconductor Corporation. All Rights Reserved.
+ # This software, including source code, documentation and related
+ # materials ("Software"), is owned by Cypress Semiconductor Corporation
+ # or one of its subsidiaries ("Cypress") and is protected by and subject to
+ # worldwide patent protection (United States and foreign),
+ # United States copyright laws and international treaty provisions.
+ # Therefore, you may use this Software only as provided in the license
+ # agreement accompanying the software package from which you
+ # obtained this Software ("EULA").
+ # If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+ # non-transferable license to copy, modify, and compile the Software
+ # source code solely for use in connection with Cypress's
+ # integrated circuit products. Any reproduction, modification, translation,
+ # compilation, or representation of this Software except as specified
+ # above is prohibited without the express written permission of Cypress.
+ #
+ # Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+ # EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+ # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+ # reserves the right to make changes to the Software without notice. Cypress
+ # does not assume any liability arising out of the application or use of the
+ # Software or any product or circuit described in the Software. Cypress does
+ # not authorize its products for use in any products where a malfunction or
+ # failure of the Cypress product may reasonably be expected to result in
+ # significant property damage, injury or death ("High Risk Product"). By
+ # including Cypress's product in a High Risk Product, the manufacturer
+ # of such system or application assumes all risk of such use and in doing
+ # so agrees to indemnify Cypress against all liability.
 #
-PLATFORM_NAME            := CY943907AEVAL1F
+
+PLATFORM_NAME            := CYW943907AEVAL1F
 WLAN_CHIP                := 43909
 WLAN_CHIP_REVISION       := B0
 WLAN_CHIP_FAMILY         := 4390x
@@ -28,10 +57,15 @@ NAME                     := Platform_$(PLATFORM_NAME)_$(BOARD_REVISION)_$(APPS_C
 
 PLATFORM_SUPPORTS_BUTTONS := 1
 
-PLATFORM_NO_USB_HOST := 1
+WICED_USB_SUPPORT    := yes
 PLATFORM_NO_DDR      := 1
 
 GLOBAL_DEFINES += SFLASH_SUPPORT_MACRONIX_PARTS
+GLOBAL_DEFINES += USING_EXTERNAL_ADC
+
+ifneq (,$(findstring USING_EXTERNAL_ADC, $(GLOBAL_DEFINES)))
+$(NAME)_COMPONENTS += drivers/adc/MAX11615
+endif
 
 PLATFORM_SOURCES := $(SOURCE_ROOT)platforms/$(PLATFORM_DIRECTORY)/
 
