dbf21e6fc13f388f8849aa99643796d3a0c88353
Greg Landry <gjl@cypress.com>
2017-10-30 08:54:07 -0400

    Add version 1 platform files. Update schematics to latest version.

---
+++ Platforms/WW101_1_CYW943907AEVAL1F/platform_config.h
@@ -0,0 +1,29 @@
+/*
+ * $ Copyright Broadcom Corporation $
+ */
+
+/*
+ * Defines internal configuration of the BCM94907AEVAL1F board
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Below configuration file defines default BSP settings.
+ * Put here platform specific configuration parameters to override these default ones.
+ */
+#include "platform_config_bsp_default.h"
+
+/*
+ * Below configuration file defines default WICED settings.
+ * To change settings replace below included file with its contents.
+ */
+#include "platform_config_wiced_default.h"
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
